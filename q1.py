5cd9eeb5f1959585fc4f8b991bf368a73ee11999
Inder-singh001 <106738356+Inder-singh001@users.noreply.github.com>
2023-01-29 18:56:23 +0530

    update

---
+++ q1.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+import pip
+
+n=int(input())
+if n%2==0:
+     n in range (0,20)
+{ 
+            if (n>2) & (n<5):
+            print("Not Weird")
+            else:
+            print("Weird")
+}       
+else:
+    print("Weird")
